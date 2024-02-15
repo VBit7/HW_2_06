6af9d802fdec4d32f3e73ba7f45f310cb361a725
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-15 17:16:38 +0200

    added basic faker for teachers

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from faker import Faker
 
 DATABASE = './college.db'
 SQL_PATH = './sql/'
+NEW_TABLES = True
+
+NUMBER_TEACHERS = 5
+NUMBER_SUBJECTS = 8
+NUMBER_GROUPS = 3
+NUMBER_STUDENTS = 50
+NUMBER_GRADES = 20
 
 
 @contextmanager
@@ -22,9 +29,10 @@ def execute_script(conn, create_table_sql):
     except sqlite3.Error as e:
         print(e)
 
-def create_tables(conn):
+
+def manage_db(conn, filename):
     try:
-        with open(f"{SQL_PATH}create_all_tables.sql", "r") as f:
+        with open(f"{SQL_PATH}{filename}", "r") as f:
             sql = f.read()
         execute_script(conn, sql)
         return 0
@@ -32,22 +40,75 @@ def create_tables(conn):
         print(f"Error: An exception occurred - {type(e).__name__}: {e}")
         return -1
 
-def generate_fake_data(conn):
-    fake = Faker()
 
-    # for table: Teachers
-    for _ in range(5):
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        insert_sql = f"INSERT INTO Teachers (FirstName, LastName) VALUES ('{first_name}', '{last_name}')"
-        execute_script(conn, insert_sql)
+def generate_fake_data(number_teachers, number_subjects, number_groups, number_students, number_grades):
+    fake_teachers = []
+    fake_subjects = []
+    fake_groups = []
+    fake_students = []
+    fake_grades = []
+
+    fake_data = Faker()
+
+    for _ in range(number_teachers):
+        # fake_teachers.append(fake_data.first_name(), fake_data.last_name())
+        fake_teachers.append(fake_data.last_name())
+
+    return fake_teachers, fake_subjects, fake_groups, fake_students, fake_grades
+
+
+def prepare_data(teachers, subjects, groups, students, grades):
+    for_teachers = []
+    for_subjects = []
+    for_groups = []
+    for_students = []
+    for_grades = []
+
+    for teacher in teachers:
+        for_teachers.append((teacher, ))
+
+    return for_teachers, for_subjects, for_groups, for_students, for_grades
+
+
+def insert_data_to_bd(teachers, subjects, groups, students, grades):
+
+    with sqlite3.connect(DATABASE) as con:
+        cur = con.cursor()
+        sql_to_teachers = "INSERT INTO Teachers (LastName) VALUES (?)"
+        cur.executemany(sql_to_teachers, teachers)
+
+        con.commit()
+
+
+# def generate_fake_data(conn):
+#     fake = Faker()
+#
+#     # for table: Teachers
+#     for _ in range(5):
+#         first_name = fake.first_name()
+#         last_name = fake.last_name()
+#         insert_sql = f"INSERT INTO Teachers (FirstName, LastName) VALUES ('{first_name}', '{last_name}')"
+#         execute_script(conn, insert_sql)
+
 
 if __name__ == '__main__':
 
     with create_connection(DATABASE) as conn:
         if conn is not None:
-            create_tables(conn)
-            generate_fake_data(conn)
+            if NEW_TABLES:
+                manage_db(conn, 'drop_all_tables.sql')
+            manage_db(conn, 'create_all_tables.sql')
+            # generate_fake_data(conn)
+
+            teachers, subjects, groups, students, grades = prepare_data(*generate_fake_data(
+                NUMBER_TEACHERS, NUMBER_SUBJECTS, NUMBER_GROUPS, NUMBER_STUDENTS, NUMBER_GRADES)
+            )
+
+            print(teachers)
+
+            # insert_data_to_bd(teachers, subjects, groups, students, grades)
+
+
         else:
             print("Error: Cannot create the database connection.")
 
