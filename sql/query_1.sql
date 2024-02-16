1c64dcbe297b5cd9f2af1f3ee895e526ab3e57ba
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-16 15:57:21 +0200

    completed query_1.sql

---
+++ sql/query_1.sql
@@ -0,0 +1,9 @@
+-- 5 студентів із найбільшим середнім балом з усіх предметів
+SELECT FirstName || ' ' || LastName AS 'Student''s name', ROUND(AvgScore, 2) AS 'Average score'
+FROM Students s
+	LEFT JOIN (
+		SELECT StudentId, AVG(Score) AS AvgScore
+		FROM Grades g
+		GROUP BY StudentId) a ON s.StudentId = a.StudentId
+ORDER BY AvgScore DESC
+LIMIT 5;
