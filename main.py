d7d777875227ccbf87e0dea231351c2f492358fa
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-16 11:42:24 +0200

    implemented data insertion into the database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
 import sqlite3
 from contextlib import contextmanager
+from random import randint, choice
 from faker import Faker
 
 DATABASE = './college.db'
@@ -30,6 +32,15 @@ def execute_script(conn, create_table_sql):
         print(e)
 
 
+def insert_script(conn, insert_sql, data_sql):
+    try:
+        c = conn.cursor()
+        c.executemany(insert_sql, data_sql)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+
 def manage_db(conn, filename):
     try:
         with open(f"{SQL_PATH}{filename}", "r") as f:
@@ -46,49 +57,71 @@ def generate_fake_data(number_teachers, number_subjects, number_groups, number_s
     fake_subjects = []
     fake_groups = []
     fake_students = []
-    fake_grades = []
+    fake_dates = []
+    start_date = datetime.date(2023, 1, 1)
+    end_date = datetime.date(2023, 12, 31)
 
     fake_data = Faker()
 
     for _ in range(number_teachers):
-        # fake_teachers.append(fake_data.first_name(), fake_data.last_name())
-        fake_teachers.append(fake_data.last_name())
+        fake_teachers.append([fake_data.first_name(), fake_data.last_name()])
 
-    return fake_teachers, fake_subjects, fake_groups, fake_students, fake_grades
+    for _ in range(number_subjects):
+        fake_subjects.append(fake_data.job())
+
+    for _ in range(number_groups):
+        fake_groups.append(fake_data.company())
+
+    for _ in range(number_students):
+        fake_students.append([fake_data.first_name(), fake_data.last_name()])
+
+    for _ in range(1, number_students * number_subjects * number_grades + 1):
+        fake_dates.append(fake_data.date_time_between(start_date=start_date, end_date=end_date))
+
+    return fake_teachers, fake_subjects, fake_groups, fake_students, fake_dates
 
 
-def prepare_data(teachers, subjects, groups, students, grades):
+def prepare_data(teachers, subjects, groups, students, fake_dates):
     for_teachers = []
     for_subjects = []
     for_groups = []
     for_students = []
     for_grades = []
 
-    for teacher in teachers:
-        for_teachers.append((teacher, ))
+    for first_name, last_name in teachers:
+        for_teachers.append((first_name, last_name))
+
+    for subject_name in subjects:
+        for_subjects.append((subject_name, randint(1, NUMBER_TEACHERS)))
+
+    for group in groups:
+          for_groups.append((group, ))
+
+    for first_name, last_name in students:
+        for_students.append((first_name, last_name, randint(1, NUMBER_GROUPS)))
+
+    for fdate in fake_dates:
+        for_grades.append((randint(1, NUMBER_STUDENTS), randint(1, NUMBER_SUBJECTS), randint(50, 100), fdate.isoformat()))
 
     return for_teachers, for_subjects, for_groups, for_students, for_grades
 
 
-def insert_data_to_bd(teachers, subjects, groups, students, grades):
+def insert_data_to_bd(conn, teachers, subjects, groups, students, grades):
 
-    with sqlite3.connect(DATABASE) as con:
-        cur = con.cursor()
-        sql_to_teachers = "INSERT INTO Teachers (LastName) VALUES (?)"
-        cur.executemany(sql_to_teachers, teachers)
+    sql_to_teachers = "INSERT INTO Teachers (FirstName, LastName) VALUES (?, ?)"
+    insert_script(conn, sql_to_teachers, teachers)
 
-        con.commit()
+    sql_to_subjects = "INSERT INTO Subjects (SubjectName, TeacherId) VALUES (?, ?)"
+    insert_script(conn, sql_to_subjects, subjects)
 
+    sql_to_groups = "INSERT INTO Groups (GroupName) VALUES (?)"
+    insert_script(conn, sql_to_groups, groups)
 
-# def generate_fake_data(conn):
-#     fake = Faker()
-#
-#     # for table: Teachers
-#     for _ in range(5):
-#         first_name = fake.first_name()
-#         last_name = fake.last_name()
-#         insert_sql = f"INSERT INTO Teachers (FirstName, LastName) VALUES ('{first_name}', '{last_name}')"
-#         execute_script(conn, insert_sql)
+    sql_to_students = "INSERT INTO Students (FirstName, LastName, GroupId) VALUES (?, ?, ?)"
+    insert_script(conn, sql_to_students, students)
+
+    sql_to_grades = "INSERT INTO Grades (StudentId, SubjectId, Score, ScoreDate) VALUES (?, ?, ?, ?)"
+    insert_script(conn, sql_to_grades, grades)
 
 
 if __name__ == '__main__':
@@ -97,17 +130,14 @@ if __name__ == '__main__':
         if conn is not None:
             if NEW_TABLES:
                 manage_db(conn, 'drop_all_tables.sql')
+
             manage_db(conn, 'create_all_tables.sql')
-            # generate_fake_data(conn)
 
             teachers, subjects, groups, students, grades = prepare_data(*generate_fake_data(
                 NUMBER_TEACHERS, NUMBER_SUBJECTS, NUMBER_GROUPS, NUMBER_STUDENTS, NUMBER_GRADES)
             )
 
-            print(teachers)
-
-            # insert_data_to_bd(teachers, subjects, groups, students, grades)
-
+            insert_data_to_bd(conn, teachers, subjects, groups, students, grades)
 
         else:
             print("Error: Cannot create the database connection.")
