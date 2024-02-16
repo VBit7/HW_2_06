5866fec40510ebb0c4b4ba254201a7319f625081
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-16 18:15:33 +0200

    completed query_3.sql

--- sql/query_3.sql
+++ sql/query_3.sql
@@ -1,1 +1,9 @@
 -- Середній бал у групах з певного предмета
+
+SELECT g.GroupName AS 'Group', s2.SubjectName AS 'Subject', ROUND(AVG(g2.Score), 2) AS 'Score'
+FROM Groups g 
+	LEFT JOIN Students s ON g.GroupId =s.GroupId 
+	LEFT JOIN Grades g2 ON s.StudentId = g2.StudentId 
+	LEFT JOIN Subjects s2 ON g2.SubjectId = s2.SubjectId 
+WHERE s2.SubjectId = 1
+GROUP BY g.GroupName, s2.SubjectName
